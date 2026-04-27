3903ddd476d15a2da6e9805981843f22321c8cab
matiibuffaRC <matbuffautn05@gmail.com>
2026-04-27 17:53:15 -0300

    Obtencion de datos exitosa, falta mejorar MUCHO

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,6 +5,20 @@ const app = express();
 const PUERTO = process.env.PORT || 3000;
 // 
 
+// Dependencias de postgreSQL
+import pkg from 'pg';
+const { Pool } = pkg;
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'CasaBuffa',
+    password: '1889',
+    port: 5432,
+});
+
+// 
+
 
 app.use(cors()); // Habilitamos CORS para permitir solicitudes desde el frontend
 app.use(express.json()); // Middleware para parsear JSON
@@ -13,10 +27,15 @@ app.get('/', (request, response) => {
     response.send('El servidor está funcionando correctamente!');
 })
 
-app.get('/products', (request, response) => {
-    response.json(products);
+app.get('/productos', async (request, response)=>{
+    try{
+        const responseDB = await pool.query('SELECT * FROM productos');
+        response.json(responseDB.rows);
+    }catch (error){
+        console.error('Error al obtener los productos: ', error);
+        response.status(500).json({ error: 'Error al obtener los productos' });
+    }
 })
-
 
 app.listen(PUERTO, () => {
     console.log(`Servidor escuchando en el puerto ${PUERTO}`);
